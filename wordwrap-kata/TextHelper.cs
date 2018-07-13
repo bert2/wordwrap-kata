ea2148fc8c0d40b9b102896b671b5f64336cee4e
bert2 <shuairan@gmail.com>
2018-07-13 20:06:45 +0200

    renamings

--- wordwrap-kata/TextHelper.cs
+++ wordwrap-kata/TextHelper.cs
@@ -7,9 +7,9 @@ namespace wordwrap_kata {
         public static string Wrap(this string text, int columns) => text?
             .GetWords()
             .BreakLongWords(columns)
-            .CombineIntoLines(columns)
-            .PadEndings(columns)
-            .Join(Environment.NewLine);
+            .CombineWordsIntoLines(columns)
+            .PadLineEndings(columns)
+            .JoinLines(Environment.NewLine);
 
         private static IEnumerable<string> GetWords(this string text) => text.Split(' ');
 
@@ -20,7 +20,7 @@ namespace wordwrap_kata {
             ? new[] { word }
             : new[] { word.Substring(0, maxLength) }.Concat(BreakWordIfTooLong(word.Substring(maxLength), maxLength));
 
-        private static IEnumerable<string> CombineIntoLines(this IEnumerable<string> words, int columns) {
+        private static IEnumerable<string> CombineWordsIntoLines(this IEnumerable<string> words, int columns) {
             var wordsQ = new Queue<string>(words);
             var line = "";
 
@@ -39,9 +39,9 @@ namespace wordwrap_kata {
 
         private static string Append(this string line, string word) => line.Length > 0 ? $"{line} {word}" : word;
 
-        private static IEnumerable<string> PadEndings(this IEnumerable<string> lines, int length) => lines
+        private static IEnumerable<string> PadLineEndings(this IEnumerable<string> lines, int length) => lines
             .Select(l => l.PadRight(length));
 
-        private static string Join(this IEnumerable<string> lines, string separator) => string.Join(separator, lines);
+        private static string JoinLines(this IEnumerable<string> lines, string separator) => string.Join(separator, lines);
     }
 }
