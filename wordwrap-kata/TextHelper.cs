3147915cdde946fa0a3a6497ac6a5f4988e827f6
bert2 <shuairan@gmail.com>
2018-07-28 15:24:37 +0200

    use aggregate to accumulate lines

--- wordwrap-kata/TextHelper.cs
+++ wordwrap-kata/TextHelper.cs
@@ -20,24 +20,20 @@ namespace wordwrap_kata {
             ? new[] { word }
             : new[] { word.Substring(0, maxLength) }.Concat(BreakWordIfTooLong(word.Substring(maxLength), maxLength));
 
-        private static IEnumerable<string> CombineWordsIntoLines(this IEnumerable<string> words, int columns) {
-            var wordsQ = new Queue<string>(words);
-            var line = "";
-
-            while (wordsQ.Any()) {
-                if (line.Append(wordsQ.Peek()).Length <= columns) {
-                    line = line.Append(wordsQ.Dequeue());
-                }
-                else {
-                    yield return line;
-                    line = "";
-                }
-            }
-
-            yield return line;
-        }
+        private static IEnumerable<string> CombineWordsIntoLines(this IEnumerable<string> words, int columns) => words
+            .Aggregate(
+                new List<string> { string.Empty },
+                (lines, word) => {
+                    if (lines.Last().Append(word).Length <= columns)
+                        lines.AppendToLast(word);
+                    else
+                        lines.Add(word);
+                    return lines;
+                });
 
         private static string Append(this string line, string word) => line.Length > 0 ? $"{line} {word}" : word;
+
+        private static void AppendToLast(this IList<string> lines, string word) => lines[lines.Count - 1] = lines[lines.Count - 1].Append(word);
 
         private static IEnumerable<string> PadLineEndings(this IEnumerable<string> lines, int length) => lines
             .Select(l => l.PadRight(length));
